27bfc05f2840a8dc037826aa4c75b81af130d59e
Valtteri Juvonen <valtteri.juvone@hotmail.com>
2023-10-19 18:14:50 +0300

    Add missing button

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import ch from '../public/ch.png'
 import cw from '../public/cw.avif'
 import ir from '../public/ir.avif'
 import arrowright from '../public/arrowright.svg'
+import blackarrowright from '../public/blackarrowright.svg'
 import TestimonialSection from '@/components/TestimonialSection/TestimonialSection'
 import BuildsSection from '@/components/BuildsSection/BuildsSection'
 import StepsSection from '@/components/StepsSection/StepsSection'
@@ -19,7 +20,10 @@ export default function Home() {
           <div className=' relative z-20 flex flex-col justify-center items-center text-center py-96 px-4 sm:px-8 md:px-16 text-white'>
             <h1 className=' text-7xl font-serif'>Arkansas Diamond Standard Homes & Interiors</h1>
             <h3 className=' mt-10 max-w-[966px] text-3xl font-sans'>Where unparalleled craftsmanship meets Southern charm. Dive into a legacy of luxury - affordable, honest and uniquely yours.</h3>
-            <button className=' mt-10 text-2xl text-black py-3 px-4 rounded-full bg-white font-sans font-semibold'>GET STARTED</button>
+            <button className=' mt-10 text-2xl text-black py-3 px-4 gap-3 flex flex-row justify-center items-center rounded-full bg-white font-sans font-semibold'>
+              GET STARTED
+              <Image src={blackarrowright} alt='black arrow pointing right' height={18} />
+            </button>
           </div>
        </section>
        <section className=' py-28 px-32 flex flex-col justify-center items-center'>
@@ -28,6 +32,10 @@ export default function Home() {
             <Image height={35} src={line} alt='line' />
             <h4 className=' text-white text-5xl mt-9 font-serif'>ROSAMOND CONSTRUCTION</h4>
             <p className=' text-white text-xl mt-9 font-sans'>From the heart of Springdale, Arkansas, Rosamond Construction upholds the essence of Southern craftsmanship and values. We're more than just builders; we're neighbors dedicated to integrity and community. We offer more than upscale, quality construction; we provide homes where families can grow, and memories are forged. From our family to yours, we promise affordability without compromising on luxury. At Rosamond Construction, your dream home isn't just a project—it's a commitment, a legacy. Let's craft homes filled with heart and soul, together.</p>
+            <button className=' mt-10 border-2 border-white text-xl text-white py-3 px-4 gap-3 flex flex-row justify-center items-center rounded-full bg-transparent font-sans font-semibold'>
+              LEARN MORE
+              <Image src={arrowright} alt='arrow pointing right' height={18} />
+            </button>
           </div>
        </section>
        <section className=' flex px-4 sm:px-8 md:px-16  flex-col w-full justify-center items-center'>
