197cbe5f4cd3bf8a1e2267d46dca0dab9b03caad
Valtteri Juvonen <valtteri.juvone@hotmail.com>
2023-10-19 18:09:26 +0300

    Footer styling

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,38 +1,44 @@
 import Link from "next/link";
 import logo from '../public/logo.svg'
+import tiktok from '../public/tok.svg'
+import facebook from '../public/fb.svg'
+import linkedin from '../public/link.svg'
+import instagram from '../public/ig.svg'
 import Image from "next/image";
 
 export default function Footer() {
   return (
-    <footer className=" pt-20 flex flex-row">
-        <div className="pb-20 px-20 flex flex-col justify-center items-center">
-            <Image height={130} src={logo} alt="logo" />
-            <div className="flex flex-row gap-3 mt-6">
-                <div className=" w-5 h-5 bg-gray-300 rounded-full"></div>
-                <div className=" w-5 h-5 bg-gray-300 rounded-full"></div>
-                <div className=" w-5 h-5 bg-gray-300 rounded-full"></div>
-                <div className=" w-5 h-5 bg-gray-300 rounded-full"></div>
-            </div>
-        </div>
-        <div className="w-full h-full pl-20 flex flex-col justify-between">
-            <div className="flex flex-row justify-between">
-                <div className="flex flex-col">
-                    <Link className=" font-sans font-semibold" href={'/'}>Home</Link>
-                    <Link className=" font-sans font-semibold" href={'/aboutus'}>About Us</Link>
-                    <Link className=" font-sans font-semibold" href={'/portfolio'}>Portfolio</Link>
-                </div>
-                <div className="flex flex-col">
-                    <Link className=" font-sans font-semibold" href={'/services'}>Services</Link>
-                    <Link className=" font-sans" href={'/services'}>Custom Home Builds</Link>
-                    <Link className=" font-sans" href={'/services'}>Cabinetry</Link>
-                    <Link className=" font-sans" href={'/services'}>Woodworking</Link>
-                </div>
-                <div className="flex flex-col">
-                    <Link className=" font-sans font-semibold" href={'/contact'}>Customer Care</Link>
-                    <Link className=" font-sans" href={'/aboutus'}>Contact Us</Link>
+    <footer className=" pt-28 flex flex-row" style={{ backgroundColor: '#161616'}}>
+        <div className="flex flex-row w-full pt-14 pb-12" style={{ backgroundColor: '#1C1C1E'}}>
+            <div className="pb-20 px-20 flex flex-col justify-center items-center">
+                <Image height={130} src={logo} alt="logo" />
+                <div className="flex flex-row justify-center items-center gap-3 mt-8">
+                    <Image src={facebook} height={21} alt="facebook logo" />
+                    <Image src={instagram} height={21} alt="facebook logo" />
+                    <Image src={linkedin} height={21} alt="facebook logo" />
+                    <Image src={tiktok} height={21} alt="facebook logo" />
                 </div>
             </div>
-            <p className=" text-white">© 2023. Rosamond Construction</p>
+            <div className="w-full h-full pl-20 flex flex-col justify-between pr-24">
+                <div className="flex flex-row justify-between">
+                    <div className="flex flex-col gap-2">
+                        <Link className=" font-sans font-semibold text-lg" href={'/'}>Home</Link>
+                        <Link className=" font-sans font-semibold text-lg" href={'/aboutus'}>About Us</Link>
+                        <Link className=" font-sans font-semibold text-lg" href={'/portfolio'}>Portfolio</Link>
+                    </div>
+                    <div className="flex flex-col gap-2">
+                        <Link className=" font-sans font-semibold text-lg" href={'/services'}>Services</Link>
+                        <Link className=" font-sans text-sm" href={'/services'}>Custom Home Builds</Link>
+                        <Link className=" font-sans text-sm" href={'/services'}>Cabinetry</Link>
+                        <Link className=" font-sans text-sm" href={'/services'}>Woodworking</Link>
+                    </div>
+                    <div className="flex flex-col gap-2">
+                        <Link className=" font-sans font-semibold text-lg" href={'/contact'}>Customer Care</Link>
+                        <Link className=" font-sans text-sm" href={'/aboutus'}>Contact Us</Link>
+                    </div>
+                </div>
+                <p className=" text-white text-sm">© 2023. Rosamond Construction</p>
+            </div>
         </div>
     </footer>
   )
