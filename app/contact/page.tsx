efdfe5b30d0e20e55cb9379275e9098fa9c6cc24
Valtteri Juvonen <valtteri.juvone@hotmail.com>
2023-10-20 17:04:35 +0300

    Submit second form state

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,3 +1,4 @@
+'use client'
 import Image from "next/image"
 import phone from '../../public/phone.svg'
 import mail from '../../public/mail.svg'
@@ -7,61 +8,81 @@ import TestimonialSection from "@/components/TestimonialSection/TestimonialSecti
 import DreamFormSection from "@/components/DreamFormSection/DreamFormSection"
 import Link from "next/link"
 import GoogleMap from "@/components/GoogleMap"
+import { useState } from "react"
 
 export default function Contact() {
+    const [submitted, setSubmitted] = useState(false);
+
+    const handleSubmit = (event: any) => {
+        event.preventDefault();
+
+        console.log("Submit");
+        setSubmitted(true);
+    }
     return (
         <div style={{ backgroundColor: '#161616'}} className="flex relative flex-col items-center justify-between">
             <section className="w-full h-full px-4 sm:px-8 md:px-16 pb-24 flex justify-center items-center">
                 <div className=" max-w-[1920px] mt-32 overflow-hidden w-full h-full rounded-3xl bg-white flex flex-row">
-                    <form className=" flex flex-col justify-start items-center w-full lg:w-1/2 py-20 px-5 md:px-20 lg:px-28">
+                    <form onSubmit={handleSubmit} className=" flex flex-col justify-start items-center w-full lg:w-1/2 py-20 px-5 md:px-20 lg:px-28">
                         <div className="flex flex-col w-full max-w-[475px]">
-                            <h5 className=" text-5xl text-black font-serif pb-5">Get in Touch</h5>
-                            <p className=" text-sm text-black font-sans pb-7">Whether you're an existing customer with inquiries or someone new with a dream project in mind, we're here to listen, assist, and guide. Get in touch with Rosamond Construction today.</p>
-                            <div className="flex mb-4 rounded-lg shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-formfocus">
-                                <input
-                                    type="text"
-                                    name="username"
-                                    id="Name Lastname"
-                                    required
-                                    autoComplete="username"
-                                    className="block w-full outline-none border-0 px-2 bg-transparent py-2.5 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                                    placeholder="Name"
-                                />
+                            { submitted ? 
+                            <div className="flex flex-col">
+                                <h5 className=" text-5xl text-black font-serif pb-5">Thank you!</h5>
+                                <p className=" text-sm text-black font-sans pb-7">We will reach out within 24 to 48 hours. To connect with us sooner, call us or email us directly to the contact information listed below.</p>
                             </div>
-                            <div className="flex mb-4 rounded-lg shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-formfocus">
-                                <input
-                                    type="text"
-                                    name="username"
-                                    id="Name Lastname"
-                                    required
-                                    autoComplete="Email"
-                                    className="block w-full outline-none border-0 px-2 bg-transparent py-2.5 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                                    placeholder="Email"
-                                />
+                            :
+                            <div className="flex flex-col">
+                                <h5 className=" text-5xl text-black font-serif pb-5">Get in Touch</h5>
+                                <p className=" text-sm text-black font-sans pb-7">Whether you're an existing customer with inquiries or someone new with a dream project in mind, we're here to listen, assist, and guide. Get in touch with Rosamond Construction today.</p>
                             </div>
-                            <div className="flex mb-4 rounded-lg shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-formfocus">
-                                <input
-                                    type="text"
-                                    name="username"
-                                    id="Name Lastname"
-                                    required
-                                    autoComplete="username"
-                                    className="block w-full outline-none border-0 px-2 bg-transparent py-2.5 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                                    placeholder="Phone Number"
-                                />
+                            }
+                            <div className={`${submitted && 'hidden'} w-full flex flex-col`}>
+                                <div className="flex mb-4 rounded-lg shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-formfocus">
+                                    <input
+                                        type="text"
+                                        name="username"
+                                        id="Name Lastname"
+                                        required
+                                        autoComplete="username"
+                                        className="block w-full outline-none border-0 px-2 bg-transparent py-2.5 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                                        placeholder="Name"
+                                    />
+                                </div>
+                                <div className="flex mb-4 rounded-lg shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-formfocus">
+                                    <input
+                                        type="text"
+                                        name="username"
+                                        id="Name Lastname"
+                                        required
+                                        autoComplete="Email"
+                                        className="block w-full outline-none border-0 px-2 bg-transparent py-2.5 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                                        placeholder="Email"
+                                    />
+                                </div>
+                                <div className="flex mb-4 rounded-lg shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-formfocus">
+                                    <input
+                                        type="text"
+                                        name="username"
+                                        id="Name Lastname"
+                                        required
+                                        autoComplete="username"
+                                        className="block w-full outline-none border-0 px-2 bg-transparent py-2.5 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                                        placeholder="Phone Number"
+                                    />
+                                </div>
+                                <div className="w-full flex mb-4 rounded-lg shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-formfocus">
+                                    <input
+                                        type="text"
+                                        name="username"
+                                        id=""
+                                        required
+                                        autoComplete="username"
+                                        className="block w-full outline-none border-0 px-2 bg-transparent py-2.5 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                                        placeholder="How did you find us?"
+                                    />
+                                </div>
                             </div>
-                            <div className="w-full flex mb-4 rounded-lg shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-formfocus">
-                                <input
-                                    type="text"
-                                    name="username"
-                                    id=""
-                                    required
-                                    autoComplete="username"
-                                    className="block w-full outline-none border-0 px-2 bg-transparent py-2.5 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                                    placeholder="How did you find us?"
-                                />
-                            </div>
-                            <button type="submit" className=" rounded-full flex flex-row justify-center items-center gap-2 bg-black w-full py-3 font-sans">
+                            <button disabled={submitted} type="submit" className=" rounded-full flex flex-row justify-center items-center gap-2 bg-black disabled:bg-gray-300 w-full py-3 font-sans">
                                 SUBMIT
                                 <Image src={arrowright} alt="arrow right" height={15} />
                             </button>
